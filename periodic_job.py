6c56c0131c5cfeb0257ecb33d0144d08e8ab57c6
NKay <n.klipp@nkay.info>
2021-02-27 19:31:34 +0100

    Add logging entry for next run time of periodic job

--- periodic_job.py
+++ periodic_job.py
@@ -1,23 +1,34 @@
 import threading
 import logging
+import datetime
 
 logger = logging.getLogger()
 
 
 def schedule_function(interval: int, worker_func: callable, args: [] = None, kwargs: {} = None, iterations: int = 0):
-    if iterations != 1:
-        threading.Timer(
-            interval,
-            schedule_function, [interval, worker_func, args, kwargs, 0 if iterations == 0 else iterations-1]
-        ).start()
-    logger.info("Scheduled to run {func} at interval {interval}\n\
-     args: {args} \n\
-      kwargs: {kwargs}".format(
-        func=worker_func.__name__,
-        interval=interval,
-        args=args,
-        kwargs=kwargs)
-    )
+    try:
+        if iterations != 1:
+            threading.Timer(
+                interval,
+                schedule_function, [interval, worker_func, args, kwargs, 0 if iterations == 0 else iterations - 1]
+            ).start()
+    except Exception as e:
+        logger.error(e, exc_info=True)
+
+    try:
+        logger.info("Scheduled to run {func} at interval {interval}\n\
+         args: {args} \n\
+          kwargs: {kwargs}".format(
+            func=worker_func.__name__,
+            interval=interval,
+            args=args,
+            kwargs=kwargs)
+        )
+        time = (datetime.datetime.now() + datetime.timedelta(seconds=interval)).strftime("%Y-%m-%d-%H-%M")
+        logger.info("Next run of {func} at {time}".format(func=worker_func.__name__, time=time))
+    except Exception as e:
+        logger.error(e, exc_info=True)
+
     try:
         if args and kwargs:
             worker_func(*args, **kwargs)
