6bdd9355ecbb904ccc8e8e18a8de8c1ffc4ab585
NKay <n.klipp@nkay.info>
2021-03-08 12:23:34 +0100

    Catch case when html has no body

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup as Soup
+from logging import getLogger
 
+logger = getLogger()
 
 def soup_insert_in_html_body(html_string: str, line):
     soup = Soup(str(html_string))
@@ -13,14 +15,19 @@ def soup_insert_in_html_body(html_string: str, line):
 def soup_insert_link_in_html_body(html_string: str, link: str, text: str = None):
     soup = Soup(str(html_string), features="html.parser")
     body = soup.find('body')
-    p = soup.new_tag('p')
-    body.insert(0, p)
-    a = soup.new_tag('a')
-    a['href'] = link
-    if text:
-        a.string = str(text)
-    else:
-        a.string = str(link)
+    if body:
+        p = soup.new_tag('p')
+        body.insert(0, p)
+        a = soup.new_tag('a')
+        a['href'] = link
+        if text:
+            a.string = str(text)
+        else:
+            a.string = str(link)
 
-    p.insert(0, a)
+        p.insert(0, a)
+    else:
+        logger.warning("No body in HTML!")
+        logger.warning("DUMP HTML:\n {html}".format(html=str(soup.prettify(formatter="html5"))))
+
     return str(soup.prettify(formatter="html5"))
